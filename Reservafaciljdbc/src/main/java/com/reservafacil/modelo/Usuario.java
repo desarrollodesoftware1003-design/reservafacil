fdb258fde5b38e078513216e7c2c30fbb506a0d5
Paula <desarrollodesoftware1003@gmail.com>
2026-04-20 17:45:52 -0500

    Rename 'nombre' to 'usuario' in Usuario class

--- Reservafaciljdbc/src/main/java/com/reservafacil/modelo/Usuario.java
+++ Reservafaciljdbc/src/main/java/com/reservafacil/modelo/Usuario.java
@@ -3,14 +3,14 @@ package com.reservafacil.modelo;
 public class Usuario {
 
     private int id;
-    private String nombre;
+    private String usuario;
     private String correo;
     private String password;
 
     public Usuario() {}
 
-    public Usuario(String nombre, String correo, String password) {
-        this.nombre = nombre;
+    public Usuario(String usuario, String correo, String password) {
+        this.usuario = usuario;
         this.correo = correo;
         this.password = password;
     }
@@ -19,8 +19,8 @@ public class Usuario {
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getUsuario() { return usuario; }
+    public void setUsuario(String usuario) { this.usuario = usuario; }
 
     public String getCorreo() { return correo; }
     public void setCorreo(String correo) { this.correo = correo; }
