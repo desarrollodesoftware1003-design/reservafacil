bf831aca3274fbac2f4c5e8c6eb665cd2eb19ff1
Paula <desarrollodesoftware1003@gmail.com>
2026-04-20 17:37:35 -0500

    Update database connection URL to port 3307

--- Reservafaciljdbc/src/main/java/com/reservafacil/conexion/ConexionBD.java
+++ Reservafaciljdbc/src/main/java/com/reservafacil/conexion/ConexionBD.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConexionBD {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/reservafacil";
+    private static final String URL = "jdbc:mysql://localhost:3307/reservafacil";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
